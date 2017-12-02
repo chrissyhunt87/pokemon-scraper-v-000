5aabb7288943fda3c986e11e70ff0256a9e4859a
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-02 01:22:07 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+
+  def initialize(attributes)
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES ('#{name}', '#{type}');")
+  end
+
+  def self.find(id, db)
+    attributes = {
+      :id => db.execute("SELECT pokemon.id FROM pokemon WHERE pokemon.id == #{id};")[0],
+      :name => db.execute("SELECT pokemon.name FROM pokemon WHERE pokemon.id == #{id};")[0],
+      :type => db.execute("SELECT pokemon.type FROM pokemon WHERE pokemon.id == #{id};")[0]
+    }
+    puts attributes
+    self.new(attributes)
+  end
+
 end
